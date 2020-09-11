44e66050bf705bf3238d53b6d62a59177f954d7e
Brian Skinn <brian.skinn@gmail.com>
2020-09-11 11:18:02 -0400

    Blacken; get base code working
    Now need to shiv it

--- src/nanovea_roughness/__main__.py
+++ src/nanovea_roughness/__main__.py
@@ -1,17 +1,41 @@
 import os
 from pathlib import Path
+from time import strftime
 
 import openpyxl as opxl
 from tqdm import tqdm
 
-# from neutrino_polzn.core import NeutrinoPolzn
+from nanovea_roughness.core import (
+    NanoveaData,
+    nanovea_data_from_scanfile,
+    Sa_calc as Sa,
+    Sp_calc as Sp,
+    Sq_calc as Sq,
+    Sv_calc as Sv,
+    Sz_calc as Sz,
+)
 
 
 def main():
     base_path = Path(os.getcwd())
 
+    out_path = base_path / f"roughness_{strftime('%Y%m%d_%H%M%S')}.xlsx"
+
+    wb = opxl.Workbook()
+    ws = wb.active
+    ws.title = "Roughness Data"
+
+    ws.append(["", "(Units typically um)"])
+    ws.append(["Data File", "Sa", "Sz", "Sq", "Sp", "Sv"])
+
     for fpath in tqdm([p for p in base_path.iterdir() if p.suffix == ".txt"]):
-        print(str(fpath))
+        try:
+            data = nanovea_data_from_scanfile(fpath)[NanoveaData.ZData]
+            ws.append([fpath.name, Sa(data), Sz(data), Sq(data), Sp(data), Sv(data)])
+        except Exception as e:
+            ws.append([fpath.name, f"(processing failed: {e.__class__})"])
+
+    wb.save(str(out_path))
 
 
 if __name__ == "__main__":
--- src/nanovea_roughness/core.py
+++ src/nanovea_roughness/core.py
@@ -9,16 +9,17 @@ from scipy.optimize import minimize
 
 # #####  NANOVEA DATA IMPORT  #####
 
+
 class NanoveaData(Enum):
-    Filename = 'fname'
-    Counts = 'counts'
-    Incs = 'incs'
-    ZData = 'zdata'
+    Filename = "fname"
+    Counts = "counts"
+    Incs = "incs"
+    ZData = "zdata"
 
 
-def nanovea_data_from_scanfile(fname):
+def nanovea_data_from_scanfile(fpath):
     """Load a Nanovea data file and return structured contents."""
-    with open(fname) as f:
+    with fpath.open("r") as f:
         # Create a CSV reader from the indicated file.
         csvr = csv.reader(f)
 
@@ -37,7 +38,7 @@ def nanovea_data_from_scanfile(fname):
         arr = np.fromiter(gen(csvr), dtype=np.float)
 
     # Reshape the resulting array into three columns, x/y/z
-    arr = arr.reshape((arr.shape[0]//3, 3))
+    arr = arr.reshape((arr.shape[0] // 3, 3))
 
     if arr[0, 0] == arr[1, 0]:
         # Data was scanned along the y-axis first
@@ -59,19 +60,19 @@ def nanovea_data_from_scanfile(fname):
         # running in the innermost cycle in the data file,
         # it lies along the columns (axis=0), when it's desired
         # to lie along the rows (axis=1); see bottom
-        xgrid = arr[:,0].reshape((yct, xct), order='F').T
-        ygrid = arr[:,1].reshape((yct, xct), order='F').T
-        zgrid = arr[:,2].reshape((yct, xct), order='F').T
+        xgrid = arr[:, 0].reshape((yct, xct), order="F").T
+        ygrid = arr[:, 1].reshape((yct, xct), order="F").T
+        zgrid = arr[:, 2].reshape((yct, xct), order="F").T
     else:
         # Data was scanned along the x-axis first
         # Everything(?) here is x/y reversed from the above.
-        yct = np.sum(arr == arr[0,0], axis=0)[0]
+        yct = np.sum(arr == arr[0, 0], axis=0)[0]
         xct = arr.shape[0] // yct
         xinc = arr[1, 0] - arr[0, 0]
         yinc = arr[xct, 1] - arr[0, 1]
-        xgrid = arr[:,0].reshape((xct, yct), order='F')
-        ygrid = arr[:,1].reshape((xct, yct), order='F')
-        zgrid = arr[:,2].reshape((xct, yct), order='F')
+        xgrid = arr[:, 0].reshape((xct, yct), order="F")
+        ygrid = arr[:, 1].reshape((xct, yct), order="F")
+        zgrid = arr[:, 2].reshape((xct, yct), order="F")
 
     # At this point, -1 values are replaced with nan,
     # and the x-coordinate increases along axis=0.
@@ -81,32 +82,35 @@ def nanovea_data_from_scanfile(fname):
     # is print()'ed
     zgrid[zgrid == -1] = np.nan
 
-    out_d = {NanoveaData.Filename: fname,
-             NanoveaData.Counts: [xct, yct],
-             NanoveaData.Incs: [xinc, yinc],
-             NanoveaData.ZData: zgrid}
+    out_d = {
+        NanoveaData.Filename: fpath.name,
+        NanoveaData.Counts: [xct, yct],
+        NanoveaData.Incs: [xinc, yinc],
+        NanoveaData.ZData: zgrid,
+    }
 
     return out_d
 
 
 # #####  PLANE RESIDUALS CALCULATION  #####
 
+
 def plane_array(params, arr):
     """Helper function to generate plane data from a model fit.
-    
+
     Plane data is generated over the same grid as the input 'arr`'.
-    
+
     params[0] is the x-coefficient
     params[1] is the y-coefficient
     params[2] is the constant
-    
+
     Function is nan-safe.
-    
+
     """
-    X, Y = np.meshgrid(*map(np.arange, arr.shape), indexing='ij')
-    
-    return params[0]*X + params[1]*Y + params[2]
-    
+    X, Y = np.meshgrid(*map(np.arange, arr.shape), indexing="ij")
+
+    return params[0] * X + params[1] * Y + params[2]
+
 
 def get_residuals(arr):
     """Calculate residuals over best-fit plane.
@@ -115,116 +119,114 @@ def get_residuals(arr):
     are uniform.
 
     """
+
     def fit_resids(params, arr):
         """Helper function to do the fitting.
-        
+
         params[0] is the x-coefficient
         params[1] is the y-coefficient
         params[2] is the constant
-        
+
         Returns the array of residuals.
-        
+
         Function is nan-safe.
-        
+
         """
         return arr - plane_array(params, arr)
-    
-    
+
     def fit_func(params, arr):
         """Helper function to do the fitting.
-        
+
         params[0] is the x-coefficient
         params[1] is the y-coefficient
         params[2] is the constant
-        
+
         Fit function is the SSD from data to plane.
-        
+
         Function is nan-safe.
-        
+
         """
         return np.nansum(fit_resids(params, arr) ** 2)
-    
-    res = minimize(fit_func, [0, 0, 0], args=(arr,), method='Nelder-Mead')
-    
+
+    res = minimize(fit_func, [0, 0, 0], args=(arr,), method="Nelder-Mead")
+
     return fit_resids(res.x, arr), res.x
 
 
 # #####    ROUGHNESS CALCULATIONS  #####
 
+
 def Sa_calc(arr):
     """Calculate the Sa of the given array over a best-fit plane.
-    
+
     Sa is the mean of the absolute residuals relative to the fitted surface.
-    
+
     ASSUMES x- and y-spacings are (possibly different) CONSTANTS.
-    
+
     Function is nan-safe.
-    
+
     """
-    resids = get_residuals(arr)
-    
+    resids = get_residuals(arr)[0]
+
     return simps(simps(np.nan_to_num(np.abs(resids)))) / np.sum(np.isfinite(resids))
 
 
 def Sq_calc(arr):
     """Calculate the Sq of the given array over a best-fit plane.
-    
+
     Sq is the RMS value of the residuals relative to the fitted surface.
-    
+
     ASSUMES x- and y-spacings are (possibly different) CONSTANTS.
-    
+
     Function is nan-safe.
-    
+
     """
-    resids = get_residuals(arr, x, y, **kwargs)
-    
+    resids = get_residuals(arr)[0]
+
     return np.sqrt(np.nansum(resids ** 2.0) / np.sum(np.isfinite(resids)))
 
 
 def Sz_calc(arr):
     """Calculate the Sz of the given array over a best-fit plane.
-    
+
     Sz is the difference between the maximum and minimum signed residuals
     relative to the fitted surface.
-    
+
     ASSUMES x- and y-spacings are (possibly different) CONSTANTS.
-    
+
     Function is nan-safe.
-    
+
     """
-    resids = get_residuals(arr, x, y, **kwargs)
-    
+    resids = get_residuals(arr)[0]
+
     return np.nanmax(resids) - np.nanmin(resids)
 
 
 def Sp_calc(arr):
     """Calculate the Sp of the given array over a best-fit plane.
-    
+
     Sp is the maximum signed residual relative to the fitted surface.
-    
+
     ASSUMES x- and y-spacings are (possibly different) CONSTANTS.
-    
+
     Function is nan-safe.
-    
+
     """
-    resids = get_residuals(arr, x, y, **kwargs)
-    
+    resids = get_residuals(arr)[0]
+
     return np.nanmax(resids)
 
 
 def Sv_calc(arr):
     """Calculate the Sv of the given array over a best-fit plane.
-    
+
     Sv is the absolute value of the most-negative residual relative to the fitted surface.
-    
+
     ASSUMES x- and y-spacings are (possibly different) CONSTANTS.
-    
+
     Function is nan-safe.
-    
+
     """
-    resids = get_residuals(arr, x, y, **kwargs)
-    
+    resids = get_residuals(arr)[0]
+
     return np.abs(np.nanmin(resids))
-
-
-
